444d079885a573ff81658f6a0c42060135f67f41
SameerS <samant_sameer@hotmail.com>
2017-01-10 22:04:38 +0530

    Changes to make two back to back async callbacks

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* > Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -39,7 +39,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -53,10 +53,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /*  Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /*  Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -66,7 +66,7 @@ $(function() {
             expect($('body')).toHaveClass("menu-hidden");
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /*  Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -75,16 +75,16 @@ $(function() {
         it('changes visibility on click', function() {
             var btn;
             btn = $('.menu-icon-link');
-            expect($('body')).toHaveClass("menu-hidden");
             btn.click();
             expect($('body')).not.toHaveClass("menu-hidden");
             btn.click();
+            expect($('body')).toHaveClass("menu-hidden");
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /*  Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /*  Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -94,35 +94,33 @@ $(function() {
             loadFeed(0, function() { done(); });
         });
 
-        it('has entry element', function(done) {
-            expect($('.feed')).toContainElement($('.entry-link'));
-            done();
+        it('has entry element', function() {
+            expect($('.feed')).toContainElement($('.entry'));
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection"*/
+    /*  Write a new test suite named "New Feed Selection"*/
     describe('New Feed Selection', function() {
         var oldFeed;
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /*  Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        //SS: get first non current feed to call
-
-        beforeEach(function(done) {
-            var title = $('.header-title').text();
-            oldFeed = $('.feed').html();
-            for (var i = 0; i < allFeeds.length; i++) {
-                if (allFeeds[i].name != title) {
-                    loadFeed(i, function() { done(); });
-                    break;
-                } else if (i == allFeeds.length - 1) { expect(true).toBe(false); } // test dose not pass something wrong if loop completes and no existing feed loaded
-            }
+        //SS: call first load
+        beforeAll(function(done) {
+            loadFeed(0, function() { done(); });
         });
 
-        it('loads new feed', function(done) {
+        //SS: call second load
+        it('loads feed', function(danadone) {
+            expect($('.feed')).toContainElement($('.entry'));
+            oldFeed = $('.feed').html();
+            loadFeed(1, function() { danadone(); });
+        });
+
+        //SS: test 
+        it('loads new feed', function() {
             expect($('.feed').html()).not.toEqual(oldFeed);
-            done();
         });
     });
 }());
